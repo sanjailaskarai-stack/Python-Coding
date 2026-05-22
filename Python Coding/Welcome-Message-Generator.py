864d0b694cdab68adb535fed83994eaacfee80ec
Sanjai Lashkarai <sanjailaskarai@gmail.com>
2026-05-22 11:54:22 +0530

    Create Welcome-Message-Generator.py

---
+++ Python Coding/Welcome-Message-Generator.py
@@ -0,0 +1,13 @@
+# Welcome Message Generator
+
+# Step 1: Ask for user details
+name = input("What's your name? ").lower()
+hobby = input("What's your favorite hobby? ").lower()
+color = input("What's your favorite color? ").lower()
+# Step 2: Generate a personalized welcome message
+print("\n--- Welcome Message ---")
+print(f"Hello, {name}.! 👋")
+print(f"Welcome to the world of Python Programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"This is your favorite color,{color}")
+print(f"Get ready to build something amazing today.")
